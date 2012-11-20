f761d3e3762efdf854ee0973a76294ec98abff0a
pelesao <pelesao@a0ee1c51-3a0f-b09b-22f5-d6d47cc2f27c>
2012-11-20 03:11:25 +0000

    Saída do último estado inconsistente. Funcionando erroneamente. Adicionado arquivo para teste de comandos. Adicionadas as funções geradoras de Label.

---
+++ Compiladores/Compiladores/ATWLabelManager.h
@@ -0,0 +1,33 @@
+#ifndef ATWLABEL
+#define ATWLABEL
+//-----------------------------------------------------------------------------------------------------
+#include "GeneralHashFunctions.h"
+#include "ATW_MemoryManager.h"
+#include "EventsManager.h"
+#include "Globals.h"
+#include "ERRO_DEF.h"
+#include <queue>
+#include <stdio.h>
+#include <stdlib.h>
+//-----------------------------------------------------------------------------------------------------
+using namespace std;
+using namespace MemoryManager;
+//-----------------------------------------------------------------------------------------------------
+#endif
+//-----------------------------------------------------------------------------------------------------
+#define MAX_LABELS 10000
+//-----------------------------------------------------------------------------------------------------
+namespace LabelController{
+	//VARIABLES--------------------------------------------------
+	extern EventsManager* _ev;
+	extern int*		      _CurrentPCAdress;
+	extern int            _Labels[MAX_LABELS];
+	extern int            _RotCount;
+	extern queue<int>     _AvailableFreeLabels;
+	//VARIABLES--------------------------------------------------
+
+	//METHODS----------------------------------------------------
+	extern char*          ATWNovoRot();
+	extern void           ATWRemoveLabel(int _Label);
+	//METHODS----------------------------------------------------
+}
